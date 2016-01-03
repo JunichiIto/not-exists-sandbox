b76486790f9a4a402c80d426d5f26fbf35a441a1
Junichi Ito <jit@sonicgarden.jp>
2016-01-04 07:23:43 +0900

    Add scope

--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -2,4 +2,25 @@ class Phrase < ActiveRecord::Base
   has_many :translatabilities
   has_many :translations, through: :translatabilities
   has_many :voices
+
+  # 特定のPhraseのTranslationが存在し、そのVoiceが一つもない要素を取得したい
+  scope :with_translation_without_voice, -> {
+    query = <<-SQL
+EXISTS (
+  SELECT *
+  FROM translatabilities ta
+  INNER JOIN translations t
+  ON t.id = ta.translation_id
+  WHERE
+  ta.phrase_id = phrases.id
+)
+AND NOT EXISTS (
+  SELECT *
+  FROM voices v
+  WHERE
+  v.phrase_id = phrases.id
+)
+    SQL
+    where(query)
+  }
 end
--- test/models/phrase_test.rb
+++ test/models/phrase_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class PhraseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '特定のPhraseのTranslationが存在し、そのVoiceが一つもない要素を取得したい' do
+    phrase_with_voice_and_translation = Phrase.create! name: 'Voiceあり、Translationあり'
+    translation = Translation.create! name: '翻訳１'
+    Translatability.create! phrase: phrase_with_voice_and_translation, translation: translation
+    phrase_with_voice_and_translation.voices.create! name: '音声１'
+
+    phrase_with_voice_without_translation = Phrase.create! name: 'Voiceあり、Translationなし'
+    phrase_with_voice_without_translation.voices.create! name: '音声２'
+
+    phrase_without_voice_with_translation = Phrase.create! name: 'Voiceなし、Translationあり'
+    Translatability.create! phrase: phrase_without_voice_with_translation, translation: translation
+
+    phrase_without_voice_and_translation = Phrase.create! name: 'Voiceなし、Translationなし'
+
+    phrases = Phrase.with_translation_without_voice
+    assert_equal [phrase_without_voice_with_translation.id], phrases.pluck(:id)
+  end
 end
